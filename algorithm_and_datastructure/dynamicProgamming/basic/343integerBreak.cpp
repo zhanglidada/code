e6f8364a4ec41bc04f28967a84b56f917b215e2c
zhanglidada <3063608075@qq.com>
2021-08-14 10:10:54 +0800

    8.14 at home

--- algorithm_and_datastructure/dynamicProgamming/basic/343integerBreak.cpp
+++ algorithm_and_datastructure/dynamicProgamming/basic/343integerBreak.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+// 采用动态规划的方式
+class Solution {
+  public:
+    int integerBreak(int n) {
+      vector<int> dp(n + 1, 0);
+      for (int i = 2; i <= n; i++) {
+        for (int j = 1; j < i; j++) {
+          dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]));
+        }
+      }
+      return dp[n];
+    }
+};
+
+
+
+int main() {
+  
+}
