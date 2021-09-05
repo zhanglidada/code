ac076b94c897a5b3eaac4656033a8c38795052c9
zhanglidada <3063608075@qq.com>
2021-09-05 18:10:35 +0800

    linux_net_code

---
+++ linux_net_coding/5_th/5_1_test_ending_order.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void byteorder() {
+  // 结构体中两个变量共享同一块内存
+  union
+  {
+    short value;
+    char union_bytes[sizeof(short)];
+  } test;  // 创建结构体实例名为test
+
+  test.value = 0x0102;
+  if (test.union_bytes[0] == 1 && test.union_bytes[1] == 2)
+    printf("big ending");
+  else if (test.union_bytes[1] == 1 && test.union_bytes[0] == 2)
+    printf("little ending");
+  else
+    printf("unknown");
+}
+int main() {
+  byteorder();
+  return 0;
+}
