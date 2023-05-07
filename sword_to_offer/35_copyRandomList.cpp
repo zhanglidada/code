0806e1ecc22bc5bc3b1b593c1e4b657d1da4acc2
zhanglidada <3063608075@qq.com>
2023-05-07 11:16:27 +0800

    2023/5/7

--- sword_to_offer/35_copyRandomList.cpp
+++ sword_to_offer/35_copyRandomList.cpp
@@ -37,37 +37,47 @@ class Solution {
  private:
    unordered_map<Node*, Node*> visited;  // 用于存储已经拷贝过的节点
 };
+
 // 采用广度优先进行遍历
 class Solution {
  public:
   Node* copyRandomList(Node* head) {
     return bfs(head);
   }
+
   Node* bfs(Node* head) {
     if (head == nullptr)  return head;
     queue<Node*> que;
     // 创建头节点并入队
     Node* copy = new Node(head->val);
     que.push(head);
+
+    // 插入访问map中
     visited[head] = copy;
+
     while (!que.empty()) {
+      // 先获取头节点
       Node* tmp = que.front();
       que.pop();
+
       // 若当前节点的next没有访问过，则创建新的节点并将其入队
       if (tmp->next && visited.count(tmp->next) == 0) {
-        que.push(tmp->next);
+        que.push(tmp->next);  // 入队next节点
         visited[tmp->next] = new Node(tmp->next->val);
       }
+      // 若当前节点的random没有访问过，则创建新的节点并将其入队
       if (tmp->random && visited.count(tmp->random) == 0) {
-        que.push(tmp->random);
+        que.push(tmp->random);  // 入队random节点
         visited[tmp->random] = new Node(tmp->random->val);
       }
+
       visited[tmp]->next = visited[tmp->next];
       visited[tmp]->random = visited[tmp->random];
     }
     return copy;
   }
  private:
+   // <原始链表节点，新的copy链表节点>
    unordered_map<Node*, Node*> visited;  // 用于存储已经拷贝过的节点
 };
 
@@ -82,6 +92,7 @@ class Solution {
     return relinkNodes(head);
   }
  private:
+  // 先拷贝节点，并设置拷贝节点位置在原节点之后
   void copyNode(Node* head) {
     while (head) {
       Node* copyHead = new Node(head->val);
@@ -90,6 +101,7 @@ class Solution {
       head = copyHead->next;
     }
   }
+  // 设置所有拷贝节点的random节点
   void copyRandom(Node* head) {
     while (head) {
       Node* cloneNode = head->next;
@@ -99,6 +111,7 @@ class Solution {
       head = cloneNode->next;
     }
   }
+  // 重新链接
   Node* relinkNodes(Node* head) {
     Node* cloneHead = head->next;
     Node* cloneNode = cloneHead;
