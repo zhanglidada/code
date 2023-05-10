120e097a77f35bcbefdddf98cf930d5303ba8227
zhanglidada <3063608075@qq.com>
2023-05-10 22:33:49 +0800

    2023/5/10_22.33

--- sword_to_offer/22_getKthFromEnd.cpp
+++ sword_to_offer/22_getKthFromEnd.cpp
@@ -8,14 +8,20 @@ struct ListNode {
   ListNode *next;
   ListNode(int x) : val(x), next(NULL) {}
 };
+
+// 双指针法
 class Solution {
  public:
   ListNode* getKthFromEnd(ListNode* head, int k) {
     ListNode* fast = head;
     ListNode* slow = head;
+
+    // 先移动第一个指针到第k个位置
     for (int i = 0; i < k; i++) {
         fast = fast->next;
     }
+
+    // 前后指针一起移动
     while (fast) {
       fast = fast->next;
       slow = slow->next;
