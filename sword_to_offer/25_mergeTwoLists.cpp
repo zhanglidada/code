6ff84abbfadfba3c34838626d3f19e4f855c61ae
zhanglidada <3063608075@qq.com>
2023-05-10 21:56:46 +0800

    2023-5-11_18:17

--- sword_to_offer/25_mergeTwoLists.cpp
+++ sword_to_offer/25_mergeTwoLists.cpp
@@ -28,3 +28,44 @@ class Solution {
     return dumy->next;
   }
 };
+
+// 初步优化
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        // 定义头指针
+        ListNode*   tmpnode = new ListNode(0);
+        // 冗余指针
+        ListNode*   cur = tmpnode;
+        // 提前返回
+        if (l1 == nullptr)
+            return l2;
+        if (l2 == nullptr)
+            return l1;
+
+        while (l1 && l2)
+        {
+            if (l1->val < l2->val)
+            {
+                cur->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            
+            cur = cur->next;
+            cur->next = nullptr;
+        }
+
+        if (l1 != nullptr)
+            cur->next = l1;
+
+        if (l2 != nullptr)
+            cur->next = l2;
+
+        return tmpnode->next;
+    }
+};
