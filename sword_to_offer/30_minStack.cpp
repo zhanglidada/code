a3b892de974e6e9ef29f8748974058f8ae1fd3d0
zhanglidada <3063608075@qq.com>
2023-05-16 20:33:27 +0800

    2023/5/16

--- sword_to_offer/30_minStack.cpp
+++ sword_to_offer/30_minStack.cpp
@@ -23,14 +23,21 @@ class MinStack {
   void push(int x) {
     minStack1->push(x);
     if (minStack2->empty())
+    {
       minStack2->push(x);
+    }
     else if (x <= minStack2->top())
+    {
+      // 相等时也push
       minStack2->push(x);
+    }
   }
   
   void pop() {
+    // 先出栈常规栈
     int val = minStack1->top();
     minStack1->pop();
+    // 当前值为栈中的最小值，stk2出栈
     if (val == minStack2->top())
       minStack2->pop();
   }
@@ -42,7 +49,8 @@ class MinStack {
   int min() {
     return minStack2->top();
   }
-  stack<int> *minStack1, *minStack2;
+  stack<int> *minStack1;
+  stack<int> *minStack2;  // 最小栈
 };
 
 int main () {
