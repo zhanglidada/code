7b464c9bda6859d6e56e4bdbaa98b31fa8fae405
zhanglidada <3063608075@qq.com>
2023-05-17 21:41:37 +0800

    2023/5/17_21.47

--- sword_to_offer/59_2_maxQueue.cpp
+++ sword_to_offer/59_2_maxQueue.cpp
@@ -3,31 +3,52 @@
 #include <queue>
 using namespace std;
 
+/**
+ * @brief 
+ * 利用一个单调递减的双端队列
+ */
 class MaxQueue {
  public:
   MaxQueue() {
   }
+
   int max_value() {
-    if (!deq.empty())  return deq.front();
+    // 获取deque的头元素
+    if (!deq.empty())
+      return deq.front();
+    
     return -1;
   }
+
   void push_back(int value) {
     que.push(value);
+
+    // 在单调递减的deque中插入当前元素
     while (!deq.empty() && deq.back() < value)
+    {
       deq.pop_back();
+    }
     deq.push_back(value);
   }
+
   int pop_front() {
-    if (que.empty())  return -1;
-    int ans = que.front();
+    int ans = 0;
+
+    if (que.empty())
+      return -1;
+
+    ans = que.front();
     que.pop();
+
+    // 当前出栈的元素为deque中最大元素
     if (deq.front() == ans)
       deq.pop_front();
+    
     return ans;
   }
  private:
   queue<int> que;
-  deque<int> deq;  // 单调递减
+  deque<int> deq;  // 保存到当前插入元素为止的单调递减序列
 };
 
 /**
