021381f7458e93b49e11c8f592d3186ae646e350
zhanglidada <3063608075@qq.com>
2023-05-20 09:06:47 +0800

    2023/5/20_9.06

--- sword_to_offer/29_spiralOrder.cpp
+++ sword_to_offer/29_spiralOrder.cpp
@@ -6,21 +6,36 @@ class Solution {
  public:
   vector<int> spiralOrder(vector<vector<int>>& matrix) {
     vector<int> ans;
+
     if (matrix.empty())  return ans;
+
+    // 定义矩阵访问的边界
     int up = 0, down = matrix.size() - 1, left = 0, right = matrix[0].size() - 1;
     while (true) {
-      for (int i = left; i <= right; i++)  ans.push_back(matrix[up][i]);
+      for (int i = left; i <= right; i++)
+        ans.push_back(matrix[up][i]);
+
       if (++ up > down) break;
-      for (int i = up; i <= down; i++)  ans.push_back(matrix[i][right]);
+
+      for (int i = up; i <= down; i++)
+        ans.push_back(matrix[i][right]);
+
       if (-- right < left) break;
-      for (int i = right; i >= left; i--)  ans.push_back(matrix[down][i]);
+      
+      for (int i = right; i >= left; i--)
+        ans.push_back(matrix[down][i]);
+
       if (-- down < up) break;
-      for (int i = down; i >= up; i--)  ans.push_back(matrix[i][left]);
+
+      for (int i = down; i >= up; i--)
+        ans.push_back(matrix[i][left]);
+
       if (++ left > right) break;
     }
     return ans;
   }
 };
+
 int main() {
   vector<vector<int>> matrix = {{1,2,3},{4,5,6},{7,8,9}};
   Solution ss;
--- sword_to_offer/31_validateStackSequences.cpp
+++ sword_to_offer/31_validateStackSequences.cpp
@@ -3,11 +3,18 @@
 #include <stack>
 using namespace std;
 
+/**
+ * @brief 
+ * propose：
+ *    根据栈的压入序列，判断另一个序列是否为栈的出栈序列
+ * notes:
+ *    栈中不存在重复数字
+ */
 class Solution {
  public:
   bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-    stack<int> st;
-    int i = 0;
+    stack<int> st;  // 使用一个栈模拟出栈序列
+    int i = 0;  // poped栈中访问元素位置
     for (auto num : pushed) {
       st.push(num);
       while (!st.empty() && st.top() == popped[i]) {
