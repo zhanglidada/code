b16951dda34ea7d791142e428f9fbd708c5c533a
zhanglidada <3063608075@qq.com>
2023-05-23 20:28:00 +0800

    2023/5/23_20.27

--- sword_to_offer/4_findNumberIn2DArray.cpp
+++ sword_to_offer/4_findNumberIn2DArray.cpp
@@ -2,12 +2,19 @@
 #include <vector>
 using namespace std;
 
+/**
+ * @brief 
+ * 将矩阵逆时针旋转45°，类似于二叉搜索树
+ * 对于每个节点，其左孩子更小，右孩子更大
+ */
 class Solution {
  public:
   bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
     if (matrix.empty() || matrix[0].empty())
       return false;
-    int i = 0, j = matrix[0].size() - 1;  // i行j列
+    
+    // 从最右上方开始查找
+    int i = 0, j = matrix[0].size() - 1;
     while (i <= matrix.size() - 1 && j >= 0) {
       if (matrix[i][j] == target)
         return true;
