ccb12d4aaf64b475705fc23ce44baebdd491a2a8
zhanglidada <3063608075@qq.com>
2023-05-24 22:11:57 +0800

    2023/5/24_22.11

---
+++ sword_to_offer/11_minArray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minArray(vector<int>& numbers) {
+      for (int i = 0; i < numbers.size() - 1; i++) {
+        if (numbers[i] > numbers[i + 1])
+          return numbers[i + 1];
+      }
+
+      return numbers[0];
+    }
+};
--- sword_to_offer/50_firstUniqChar.cpp
+++ sword_to_offer/50_firstUniqChar.cpp
@@ -3,21 +3,27 @@
 #include <unordered_map>
 using namespace std;
 
-// 采用hash映射
+// 采用hash映射，两次遍历字符串
 class Solution {
  public:
   char firstUniqChar(string s) {
     unordered_map<char, bool> hashmap;
+
+    // 遍历串中每一个字符，设置其unique属性
     for (auto c : s) {
       if (hashmap.count(c) == 0)
         hashmap[c] = true;
       else
         hashmap[c] = false;
     }
+
+    // 从头遍历串，找到第一个unique字符
     for (auto c : s) {
       if (hashmap[c])
         return c;
     }
+
+    // s为空串或者不存在唯一字符
     return ' ';
   }
 };
