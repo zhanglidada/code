d1d61aa0ab83a87fd8c69caa321ac8399a479d6f
zhanglidada <3063608075@qq.com>
2023-06-06 20:42:15 +0800

    2023/6/6

--- sword_to_offer/36_treeToDoublyList.cpp
+++ sword_to_offer/36_treeToDoublyList.cpp
@@ -14,7 +14,7 @@ public:
     Node(int _val) {
         val = _val;
         left = NULL;
-        right = NULL;
+        right = NULL; 
     }
 
     Node(int _val, Node* _left, Node* _right) {
@@ -24,6 +24,12 @@ public:
     }
 };
 
+/**
+ * @brief 
+ * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的循环双向链表。要求不能创建任何新的节点，只能调整树中节点指针的指向。
+ * 
+ * 使用中序遍历访问二叉树，得到升序序列
+ */
 class Solution {
  public:
   Node* treeToDoublyList(Node* root) {
@@ -35,18 +41,28 @@ class Solution {
   }
   // 中序遍历
   void dfs(Node* cur) {
-    if (cur->left)
+
+    // 递归查找最左节点
+    if (cur->left != nullptr)
       dfs(cur->left);
+
     // pre节点还没有初始化，说明此时是第一个节点
     if (pre == nullptr) {
       head = cur;
-    }  else {
+    }
+    else {
+      // pre right指针指向当前节点
       pre->right = cur;
     }
+
+    // 当前节点设置
     cur->left = pre;
     pre = cur;
+
+    // 查找右子树
     if (cur->right)
       dfs(cur->right);
   }
-  Node* pre = nullptr, *head = nullptr;
+  Node* pre = nullptr;  // des栈中当前节点的前一个节点，即当前节点左子树的最右节点
+  Node* head = nullptr;  // 返回头，实际为最左叶子节点
 };
