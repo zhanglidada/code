49a027af8796f1c5a7f062d8b4cf2c2bb6aaf523
zhanglidada <3063608075@qq.com>
2023-06-10 10:03:17 +0800

    2023/6/10_10.03

--- sword_to_offer/64_sumNums.cpp
+++ sword_to_offer/64_sumNums.cpp
@@ -2,10 +2,15 @@
 #include <vector>
 using namespace std;
 
+/**
+ * @brief 
+ * 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）
+ * 
+ */
 class Solution {
  public:
   int sumNums(int n) {
-    // 利用&&的短路特性
+    // 利用&&的短路特性,只要n>0, 就会递归计算n+sum(n - 1)
     n && (n += sumNums(n - 1));
     return n;
   }
--- sword_to_offer/68-1_lowestCommonAncestor.cpp
+++ sword_to_offer/68-1_lowestCommonAncestor.cpp
@@ -10,12 +10,22 @@ struct TreeNode {
   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+/**
+ * @brief 
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ * 
+ * 公共祖先的定义：
+ * 对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ */
+
 // 递归对其进行判断
 class Solution {
  public:
   TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
     while (root != nullptr) {
+      // p，q节点均在当前节点的左子树中
       if (root->val > p->val && root->val > q->val)  root = root->left;
+      // p，q节点均在当前节点的右子树中
       else if (root->val < p->val && root->val < q->val)  root = root->right;
       else break;
     }
@@ -39,6 +49,8 @@ class Solution {
     return root;
   }
 };
+
+
 // 采用递归的方式
 class Solution {
  public:
