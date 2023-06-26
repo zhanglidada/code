91f1a10f4cddb6a129f71b0d5c71872e79b9431f
zhanglidada <3063608075@qq.com>
2023-06-26 21:08:48 +0800

    2023/6/26_21.08

--- sword_to_offer/33_verifyPostorder.cpp
+++ sword_to_offer/33_verifyPostorder.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <deque>
+#include <stack>
+#include <queue>
 using namespace std;
 
 /**
@@ -54,13 +57,47 @@ class Solution {
 };
 
 
-
-
-
+/***************************************************************************************
+ * 2. 采用辅助单调栈
+ * 
+ * 后续遍历的倒序：[根节点 | 右子树 | 左子树]，类似于先序遍历的镜像 root->right->left
+ * 
+ * 1）构造一个单调递增的栈，记录遍历的元素
+ *    
+ * 2）在向右子树遍历的过程中，value值逐渐增大，一旦出现value值变小的情况，表示要进入左子树
+ *    记录此时的节点为N
+ * 
+ * 3）由于左子树中的节点均小于根节点，只要栈顶元素还大于当前节点N，持续出栈，
+ *    直到栈顶元素小于当前节点，或者栈顶元素为空
+ * 
+ * 4）此时最后出栈的那个元素为当前节点的根节点
+***************************************************************************************/
 // 采用辅助单调栈
 class Solution {
- public:
-  bool verifyPostorder(vector<int>& postorder) {
-    
-  }
+  public:
+    bool verifyPostorder(vector<int>& postorder) {
+        // 单调栈使用，单调递增的单调栈
+        stack<int> stk;
+
+        // 表示上一个根节点的元素，这里可以把postorder的最后一个元素root看成无穷大节点的左孩子
+        int pervElem = INT32_MAX;
+        
+        // 逆向遍历，就是先序遍历的镜像
+        for (int i = postorder.size() - 1; i>=0; i--){
+            // 左子树元素必须要小于递增栈中根节点的元素，否则就不是二叉搜索树
+            if (postorder[i] > pervElem){
+                return false;
+            }
+
+            while (!stk.empty() && postorder[i] < stk.top()){
+                // 数组元素小于单调栈的元素了，表示往左子树走了，记录下上个根节点
+                // 找到这个左子树对应的根节点，之前右子树全部弹出，不再记录，因为不可能在往根节点的右子树走了
+                pervElem = stk.top();
+                stk.pop();
+            }
+            // 这个新元素入栈
+            stk.push(postorder[i]);
+        }
+        return true;
+    }
 };
