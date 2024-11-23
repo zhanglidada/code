d74248adf0811630831558f50983d01ce22a2944
zhanglidada <3063608075@qq.com>
2024-11-23 10:31:42 +0800

    2024-11-23

--- cpp/101isSymmetric.cpp
+++ cpp/101isSymmetric.cpp
@@ -17,6 +17,28 @@ struct TreeNode {
 class Solution {
   public:
   bool isSymmetric(TreeNode* root) {
-      
+    if (root == nullptr)
+      return true;
+
+    if (root->left == nullptr && root->right == nullptr)
+      return true;
+
+    return mirror(root->left, root->right);
+  }
+
+  bool mirror(TreeNode* left, TreeNode* right) {
+    // 均为空true
+    if (left == nullptr && right == nullptr)
+      return true;
+    
+    // 一个空一个不空false
+    if (left == nullptr || right == nullptr)
+      return false;
+
+    // 值相等进一步比较
+    if (left->val == right->val)
+      return mirror(left->left, right->right) && mirror(left->right, right->left);
+
+    return false;
   }
 };
